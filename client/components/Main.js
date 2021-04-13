bd542c01970577dd0788ccf0e48d0e981348b9a4
Linda Saraguro <linda.Saraguro@gmail.com>
2021-04-12 20:38:20 -0400

    add Main

---
+++ client/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      <h4>Hello world, my name is </h4>
+      <h1>Linda Saraguro.</h1>
+      <h1>I develop Fullstack web apps.</h1>
+      <p>
+        I'm a New York-based Software Engineer who specializes in Fullstack Web
+        development using JavaScript throughout the stack.
+      </p>
+    </div>
+  )
+}
+export default Main
